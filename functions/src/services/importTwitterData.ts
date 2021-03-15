cd751b7758baf2f966f80ec18401eb4f50f43423
korede <khodedng@yahoo.com>
2021-03-15 02:39:26 +0100

    refactor data import

--- functions/src/services/importTwitterData.ts
+++ functions/src/services/importTwitterData.ts
@@ -1,16 +1,36 @@
-import {restore} from "firestore-export-import";
 import * as request from "request-promise";
+import {db} from "../config/firebase";
+import * as shortid from "shortid";
 
 type Payload = {
     url: string
 }
-const jsonToFirestore = async (urlPath: Payload ): Promise<string | void> => {
+const jsonToFirestore = async (urlPath: Payload ): Promise<any> => {
   try {
-    const backupData = await request(urlPath.url);
-    await restore(JSON.parse(backupData));
-    return "upload successful";
+    const twitterData = db.collection("dataSets");
+    const importData = await request(urlPath.url);
+    // eslint-disable-next-line max-len
+    const dataSets = JSON.parse(importData);
+    for (let index = 0; index < dataSets.length; index++) {
+      const data = dataSets[index];
+      const twitterDataObject = {
+        id: shortid.generate(),
+        source: data.source,
+        tweet: data.tweet,
+      };
+      await twitterData.add(twitterDataObject);
+    }
+    return ({
+      status: "success",
+      message: "External Data  import successfull",
+      data: dataSets,
+    });
   } catch (error) {
-    console.log(error);
+    return ({
+      status: "success",
+      message: "External Data  import Error",
+      data: error.message,
+    });
   }
 };
 

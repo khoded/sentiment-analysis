2442e4520a226e7f25f316f7c4853b9d5a3431bd
korede <khodedng@yahoo.com>
2021-03-16 07:06:48 +0100

    fixed: rename route

--- functions/src/routes/v1/routes.ts
+++ functions/src/routes/v1/routes.ts
@@ -11,7 +11,7 @@ router.get("/", (req, res) => res.status(200).send("Hey there!"));
 router.post("/analyze", analyzeText);
 router.post("/datasets", addTwitterData);
 router.post("/datasets/import", importTwitterData);
-router.post("/datasets/semantic", analyseData);
+router.post("/datasets/process", analyseData);
 router.get("/datasets", getAllEntries);
 router.patch("/datasets/:twitterDataId", updateTwitterData);
 router.delete("/datasets/:twitterDataId", deleteTwitterData);
